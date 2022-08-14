9535226a32c8e006c77f860240c906e91a39a95f
dylsonowski <michal.dyla@o2.pl>
2022-08-14 11:31:15 +0200

    Correcting building scripts

--- scripts/GenerateProject-Unix.sh
+++ scripts/GenerateProject-Unix.sh
@@ -1,14 +1,31 @@
 #!bin/sh
 
 PrintHelp() {
-    echo .
-    echo Enter 'GenerateProject-Unix.sh action'. Premake help:
-    echo .
-    ../vendor/Premake/premake5-linux --help
-}
-
-Clean() {
-    ../vendor/Premake/premake5-linux clean
+    echo.
+    echo Enter 'GenerateProject-Unix.sh platform action'.
+    echo.
+    echo PLATFORM
+    echo unix
+    echo macosx
+    echo.
+    echo ACTIONS
+    echo.
+    echo help              Print Premake help 
+    echo clean             Remove all binaries and generated files
+    echo codelite          Generate CodeLite project files
+    echo gmake             Generate GNU makefiles for POSIX, MinGW, and Cygwin
+    echo gmake2            Generate GNU makefiles for POSIX, MinGW, and Cygwin
+    echo vs2005            Generate Visual Studio 2005 project files
+    echo vs2008            Generate Visual Studio 2008 project files
+    echo vs2010            Generate Visual Studio 2010 project files
+    echo vs2012            Generate Visual Studio 2012 project files
+    echo vs2013            Generate Visual Studio 2013 project files
+    echo vs2015            Generate Visual Studio 2015 project files
+    echo vs2017            Generate Visual Studio 2017 project files
+    echo vs2019            Generate Visual Studio 2019 project files
+    echo vs2022            Generate Visual Studio 2022 project files
+    echo xcode4            Generate Apple Xcode 4 project files
+    echo.
 }
 
 if [ "$1" = "" ]; then
@@ -16,18 +33,25 @@ if [ "$1" = "" ]; then
     exit
 fi
 
-if [ "$1" = "clean" ]; then
-    Clean
-    exit
-fi
-
-if [ "$1" = "linux" ]; then
-    ../vendor/Premake/premake5-linux $2
+if [ "$1" = "unix" ]; then
+    if [ "$2" = "clean" ]; then
+        "$(dirname "$0")/../vendor/Premake/premake5-linux" clean
+    elif [ "$2" = "help" ]; then
+        "$(dirname "$0")/../vendor/Premake/premake5-linux" --help
+    else
+        "$(dirname "$0")/../vendor/Premake/premake5-linux" $2
+    fi
     exit
 elif [ "$1" = "mac" ]; then
-    ../vendor/Premake/premake5-macos $2
+    if [ "$2" = "clean" ]; then
+        "$(dirname "$0")/../vendor/Premake/premake5-macosx" clean
+    elif [ "$2" = "help" ]; then
+        "$(dirname "$0")/../vendor/Premake/premake5-macosx" --help
+    else
+        "$(dirname "$0")/../vendor/Premake/premake5-macosx" $2
+    fi
     exit
 else
-    echo Action did not recognized. This script allows building only Linux or MacOS projects.
+    echo ERROR: Action did not recognized. This script allows building only Linux or MacOS projects.
     exit
 fi
--- scripts/GenerateProject-Win.bat
+++ scripts/GenerateProject-Win.bat
@@ -2,19 +2,41 @@
 
 if "%~1" == "" goto :PrintHelp
 if "%~1" == "clean" goto :Clean
+if "%~1" == "help" goto :PremakeHelp
 
-call ..\vendor\Premake\premake5-windows.exe %1
-goto Done
+call "%~dp0\..\vendor\Premake\premake5-windows.exe" %1
+goto :Done
 
 :PrintHelp
 echo.
-echo Enter 'GenerateProject-Win.bat action'. Premake help:
+echo Enter 'GenerateProject-Win.bat action'.
 echo.
-call ..\vendor\Premake\premake5-windows.exe --help
-goto Done
+echo ACTIONS
+echo.
+echo help              Print Premake help 
+echo clean             Remove all binaries and generated files
+echo codelite          Generate CodeLite project files
+echo gmake             Generate GNU makefiles for POSIX, MinGW, and Cygwin
+echo gmake2            Generate GNU makefiles for POSIX, MinGW, and Cygwin
+echo vs2005            Generate Visual Studio 2005 project files
+echo vs2008            Generate Visual Studio 2008 project files
+echo vs2010            Generate Visual Studio 2010 project files
+echo vs2012            Generate Visual Studio 2012 project files
+echo vs2013            Generate Visual Studio 2013 project files
+echo vs2015            Generate Visual Studio 2015 project files
+echo vs2017            Generate Visual Studio 2017 project files
+echo vs2019            Generate Visual Studio 2019 project files
+echo vs2022            Generate Visual Studio 2022 project files
+echo xcode4            Generate Apple Xcode 4 project files
+echo.
+goto :Done
 
 :Clean
-call ..\vendor\Premake\premake5-windows.exe clean
-goto Done
+call "%~dp0\..\vendor\Premake\premake5-windows.exe" clean
+goto :Done
+
+:PremakeHelp
+call "%~dp0\..\vendor\Premake\premake5-windows.exe" --help
+goto :Done
 
 :Done
